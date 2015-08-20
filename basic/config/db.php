032fb83da0d82b9a0e23ef33abb7a04756411b6d
nekulin <nekulin@nekulin>
2015-08-20 23:27:43 +0300

    config

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=github_commits',
-    'username' => 'root',
-    'password' => 'petromysql',
-    'charset' => 'utf8',
-];
